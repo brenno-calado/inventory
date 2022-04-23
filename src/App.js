7cdfde743ac14da1e637fec22aa83f51ebeba0ec
Brenno Nascimento <brenno.nascimento@saudeid.com.br>
2022-04-23 16:07:13 -0300

    feat: add three dependencies

--- src/App.js
+++ src/App.js
@@ -1,39 +1,61 @@
-import React, { useContext, useEffect } from 'react';
+import { FirstPersonControls, MeshDistortMaterial, Sphere } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import React, { Suspense, useContext } from 'react';
+import './App.css';
 import Inventory from './components/Inventory';
 import InventoryContext from './context/InventoryContext';
-import './App.css';
-
 
 function App() {
-  const { items, checkInventory, viewInventory, setViewInventory } = useContext(InventoryContext);
-
-  useEffect(() => {
-    window.addEventListener('keypress', (evt) => {
+  window.addEventListener('keyup', (evt) => {
       if (evt.key === 'f') setViewInventory(!viewInventory)
-    }, [])
   })
+
+  const args = {
+    activeLook: true,
+    autoForward: false,
+    constrainVertical: false,
+    enabled: true,
+    heightCoef: 1,
+    heightMax: 1,
+    heightMin: 0,
+    heightSpeed: false,
+    lookVertical: true,
+    lookSpeed: 0.005,
+    movementSpeed: 1,
+    verticalMax: Math.PI,
+    verticalMin: 0,
+  }
+
+  const { viewInventory, setViewInventory } = useContext(InventoryContext);
   return (
     <main className="App">
-      { viewInventory ? <Inventory /> : <p>Press F to toggle Inventory</p>}
-      {items.map((item, index) => (
-        <div
-          key={index}
-          draggable
-          className='outside-item'
-          type="button"
-          onDragEnd={(e) => checkInventory(e, item)}
-          style={
-            {
-              left: 0,
-              height: `${item.size.height * 50}px`,
-              top: 0,
-              width: `${item.size.width * 50}px`,
-            }
-          }
-        >
-          { item.name }
-        </div>
-      ))}
+      <Canvas>
+        <FirstPersonControls {...args} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null} >
+          <mesh position={[-6, 0, -2]} rotation={[90, 0, 20]}>
+            <boxBufferGeometry attach="geometry" args={[3, 3, 3]} />
+            <meshLambertMaterial attach="material" color="lightcyan" />
+          </mesh>
+          <mesh position={[6, 0, -1]}>
+            <boxBufferGeometry attach="geometry" args={[4, 2, 2]} />
+            <meshNormalMaterial attach="material" />
+          </mesh>
+          <mesh position={[0, 1, 0]}>
+            <Sphere visible args={[1, 100, 100]} scale={1.2}>
+              <MeshDistortMaterial
+                color="white"
+                attach="material"
+                distort={0.4}
+                speed={1}
+                roughness={0.7}
+              />
+            </Sphere>
+          </mesh>
+        </Suspense>
+      </Canvas>
+      {viewInventory ? <Inventory /> : <p className='toggle-inventory'>Press F to toggle Inventory</p>}
     </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import Provider from './context/Provider';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+createRoot(document.getElementById('root')).render(
+  <Provider>
+    <App />
+  </Provider>
+)
